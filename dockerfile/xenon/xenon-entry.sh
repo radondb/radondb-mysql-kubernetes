39a859e515e040a21bbeda5ac498fcf149c100d3
zhyass <34016424+zhyass@users.noreply.github.com>
2021-04-14 17:11:38 +0800

    charts: add publishNotReadyAddresses param in headless service #34 [summary] We use `publishNotReadyAddresses` to be able to access pods even if the pod is not ready. When set to true, indicates that DNS implementations must publish the notReadyAddresses of subsets for the Endpoints associated with the Service.

--- dockerfile/xenon/xenon-entry.sh
+++ dockerfile/xenon/xenon-entry.sh
@@ -30,52 +30,77 @@ file_env() {
 	unset "$fileVar"
 }
 
-file_env 'HOST' $(hostname)
+build_conf() {
+	file_env 'HOST' $(hostname)
+	file_env 'MYSQL_REPL_PASSWORD' 'Repl_123'
+	file_env 'MYSQL_ROOT_PASSWORD' ''
+	file_env 'Master_SysVars' ''
+	file_env 'Slave_SysVars' ''
+	file_env 'LEADER_START_CMD' ':'
+	file_env 'LEADER_STOP_CMD' ':'
 
-file_env 'MYSQL_REPL_PASSWORD' 'Repl_123'
-file_env 'MYSQL_ROOT_PASSWORD' ''
-file_env 'Master_SysVars' ''
-file_env 'Slave_SysVars' ''
-file_env 'LEADER_START_CMD' ':'
-file_env 'LEADER_STOP_CMD' ':'
+	printf '{
+	"log": {
+		"level": "INFO"
+	},
+	"server": {
+		"endpoint": "%s:8801"
+	},
+	"replication": {
+		"passwd": "%s",
+		"user": "qc_repl"
+	},
+	"rpc": {
+		"request-timeout": 2000
+	},
+	"mysql": {
+		"admit-defeat-ping-count": 3,
+		"admin": "root",
+		"ping-timeout": 2000,
+		"passwd": "%s",
+		"host": "localhost",
+		"version": "mysql57",
+		"master-sysvars": "%s",
+		"slave-sysvars": "%s",
+		"port": 3306,
+		"monitor-disabled": true
+	},
+	"raft": {
+		"election-timeout": 10000,
+		"admit-defeat-hearbeat-count": 5,
+		"heartbeat-timeout": 2000,
+		"meta-datadir": "/var/lib/xenon/",
+		"leader-start-command": "%s",
+		"leader-stop-command": "%s",
+		"semi-sync-degrade": true,
+		"purge-binlog-disabled": true,
+		"super-idle": false
+	}
+}
+' "$HOST" "$MYSQL_REPL_PASSWORD" "$MYSQL_ROOT_PASSWORD" "$Master_SysVars" "$Slave_SysVars" "$LEADER_START_CMD" "$LEADER_STOP_CMD" > /etc/xenon/xenon.json
+}
 
-printf '{
- "log": {
-  "level": "INFO"
- },
- "server": {
-  "endpoint": "%s:8801"
- },
- "replication": {
-  "passwd": "%s",
-  "user": "qc_repl"
- },
- "rpc": {
-  "request-timeout": 2000
- },
- "mysql": {
-  "admit-defeat-ping-count": 3,
-  "admin": "root",
-  "ping-timeout": 2000,
-  "passwd": "%s",
-  "host": "localhost",
-  "version": "mysql57",
-  "master-sysvars": "%s",
-  "slave-sysvars": "%s",
-  "port": 3306,
-  "monitor-disabled": true
- },
- "raft": {
-  "election-timeout": 10000,
-  "admit-defeat-hearbeat-count": 5,
-  "heartbeat-timeout": 2000,
-  "meta-datadir": "/var/lib/xenon/",
-  "leader-start-command": "%s",
-  "leader-stop-command": "%s",
-  "semi-sync-degrade": true,
-  "purge-binlog-disabled": true,
-  "super-idle": false
- }
-}' "$HOST" "$MYSQL_REPL_PASSWORD" "$MYSQL_ROOT_PASSWORD" "$Master_SysVars" "$Slave_SysVars" "$LEADER_START_CMD" "$LEADER_STOP_CMD" > /etc/xenon/xenon.json
+ping_host(){
+	# wait for the host ready.
+	# see https://github.com/kubernetes/kubernetes/issues/92559
+	max=30
+	for i in `seq 1 $max`
+	do
+		if ping -c 1 -W 1 $HOST > /dev/null
+		then
+			echo "ping $HOST success."
+			break
+		else
+			if [ $i -eq $max ]
+			then
+				echo >&2 "cannot ping $HOST."
+				exit 1
+			fi
+			sleep 1
+		fi
+	done
+}
 
+build_conf
+ping_host
 exec "$@"
