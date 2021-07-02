ef288916bc689d251a5df4d0f66193af27df135d
13056675191 <1131648942@qq.com>
2021-07-02 17:53:11 +0800

    cluster: unit test for container,cluster #130

---
+++ cluster/container/xenon_test.go
@@ -0,0 +1,146 @@
+/*
+Copyright 2021 RadonDB.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package container
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	mysqlv1alpha1 "github.com/radondb/radondb-mysql-kubernetes/api/v1alpha1"
+	"github.com/radondb/radondb-mysql-kubernetes/cluster"
+)
+
+var (
+	xenonReplicas     int32 = 1
+	xenonMysqlCluster       = mysqlv1alpha1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "sample",
+			Namespace: "default",
+		},
+		Spec: mysqlv1alpha1.ClusterSpec{
+			PodSpec: mysqlv1alpha1.PodSpec{
+				Resources: corev1.ResourceRequirements{
+					Limits:   nil,
+					Requests: nil,
+				},
+			},
+			XenonOpts: mysqlv1alpha1.XenonOpts{
+				Image: "xenon image",
+			},
+			Replicas: &xenonReplicas,
+		},
+	}
+	testXenonCluster = cluster.Cluster{
+		Cluster: &xenonMysqlCluster,
+	}
+	xenonCase = EnsureContainer("xenon", &testXenonCluster)
+)
+
+func TestGetXenonName(t *testing.T) {
+	assert.Equal(t, "xenon", xenonCase.Name)
+}
+
+func TestGetXenonImage(t *testing.T) {
+	assert.Equal(t, "xenon image", xenonCase.Image)
+}
+
+func TestGetXenonCommand(t *testing.T) {
+	assert.Nil(t, xenonCase.Command)
+}
+
+func TestGetXenonEnvVar(t *testing.T) {
+	assert.Nil(t, xenonCase.Env)
+}
+
+func TestGetXenonLifecycle(t *testing.T) {
+	lifecycle := &corev1.Lifecycle{
+		PostStart: &corev1.Handler{
+			Exec: &corev1.ExecAction{
+				Command: []string{"sh", "-c",
+					"until (xenoncli xenon ping && xenoncli cluster add sample-mysql-0.sample-mysql.default:8801) > /dev/null 2>&1; do sleep 2; done",
+				},
+			},
+		},
+	}
+	assert.Equal(t, lifecycle, xenonCase.Lifecycle)
+}
+
+func TestGetXenonResources(t *testing.T) {
+	assert.Equal(t, corev1.ResourceRequirements{
+		Limits:   nil,
+		Requests: nil,
+	}, xenonCase.Resources)
+}
+
+func TestGetXenonPorts(t *testing.T) {
+	port := []corev1.ContainerPort{
+		{
+			Name:          "xenon",
+			ContainerPort: 8801,
+		},
+	}
+	assert.Equal(t, port, xenonCase.Ports)
+}
+
+func TestGetXenonLivenessProbe(t *testing.T) {
+	livenessProbe := &corev1.Probe{
+		Handler: corev1.Handler{
+			Exec: &corev1.ExecAction{
+				Command: []string{"pgrep", "xenon"},
+			},
+		},
+		InitialDelaySeconds: 30,
+		TimeoutSeconds:      5,
+		PeriodSeconds:       10,
+		SuccessThreshold:    1,
+		FailureThreshold:    3,
+	}
+	assert.Equal(t, livenessProbe, xenonCase.LivenessProbe)
+}
+
+func TestGetXenonReadinessProbe(t *testing.T) {
+	readinessProbe := &corev1.Probe{
+		Handler: corev1.Handler{
+			Exec: &corev1.ExecAction{
+				Command: []string{"sh", "-c", "xenoncli xenon ping"},
+			},
+		},
+		InitialDelaySeconds: 10,
+		TimeoutSeconds:      1,
+		PeriodSeconds:       10,
+		SuccessThreshold:    1,
+		FailureThreshold:    3,
+	}
+	assert.Equal(t, readinessProbe, xenonCase.ReadinessProbe)
+}
+
+func TestGetXenonVolumeMounts(t *testing.T) {
+	volumeMounts := []corev1.VolumeMount{
+		{
+			Name:      "scripts",
+			MountPath: "/scripts",
+		},
+		{
+			Name:      "xenon",
+			MountPath: "/etc/xenon",
+		},
+	}
+	assert.Equal(t, volumeMounts, xenonCase.VolumeMounts)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/radondb/radondb-mysql-kubernetes
 go 1.16
 
 require (
+	bou.ke/monkey v1.0.2
 	github.com/blang/semver v3.5.1+incompatible
 	github.com/go-ini/ini v1.62.0
 	github.com/go-sql-driver/mysql v1.6.0
@@ -11,6 +12,7 @@ require (
 	github.com/onsi/gomega v1.10.5
 	github.com/presslabs/controller-util v0.3.0-alpha.2
 	github.com/spf13/cobra v1.1.1
+	github.com/stretchr/testify v1.6.1
 	k8s.io/api v0.20.4
 	k8s.io/apimachinery v0.20.4
 	k8s.io/client-go v0.20.4
