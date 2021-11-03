61e8a36a08c4e90e972e8b6f9247c0d0ce14ff95
runkecheng <1131648942@qq.com>
2021-11-03 17:52:42 +0800

    internal: adding the basic framework code of the xenon http request. #284

---
+++ internal/xenon_executor.go
@@ -0,0 +1,88 @@
+/*
+Copyright 2021 RadonDB.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package internal
+
+import (
+	"fmt"
+	"net/http"
+
+	apiv1alpha1 "github.com/radondb/radondb-mysql-kubernetes/api/v1alpha1"
+	"github.com/radondb/radondb-mysql-kubernetes/utils"
+)
+
+type xenonExecutor struct {
+	httpExecutor
+	rootPassword string
+}
+
+type XenonExecutor interface {
+	GetRootPassword() string
+	SetRootPassword(rootPassword string)
+	RaftStatus(host string) (*apiv1alpha1.RaftStatus, error)
+	RaftTryToLeader(host string) error
+}
+
+func NewXenonExecutor() XenonExecutor {
+	return &xenonExecutor{httpExecutor: httpExecutor{Client: NewHttpClient(&http.Client{})}}
+}
+
+func (executor *xenonExecutor) GetRootPassword() string {
+	return executor.rootPassword
+}
+
+func (executor *xenonExecutor) SetRootPassword(rootPassword string) {
+	executor.rootPassword = rootPassword
+}
+
+// RaftStatus gets the raft status of incoming host through http.
+func (executor *xenonExecutor) RaftStatus(host string) (*apiv1alpha1.RaftStatus, error) {
+	req, err := NewXenonHttpRequest(NewRequestConfig(host, executor.rootPassword, utils.RaftStatus, nil))
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := executor.httpExecutor.Execute(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get raft status, err: %s", err)
+	}
+
+	var out map[string]interface{}
+	if err = utils.UnmarshalJSON(response.Body, &out); err != nil {
+		return nil, err
+	}
+
+	nodesJson := out["nodes"].([]interface{})
+	nodes := []string{}
+	for _, node := range nodesJson {
+		nodes = append(nodes, node.(string))
+	}
+	return &apiv1alpha1.RaftStatus{Role: out["state"].(string), Leader: out["leader"].(string), Nodes: nodes}, nil
+}
+
+// RaftTryToLeader try setting up incoming host to the leader node.
+func (executor *xenonExecutor) RaftTryToLeader(host string) error {
+	req, err := NewXenonHttpRequest(NewRequestConfig(host, executor.rootPassword, utils.RaftTryToLeader, nil))
+	if err != nil {
+		return err
+	}
+
+	_, err = executor.httpExecutor.Execute(req)
+	if err != nil {
+		return fmt.Errorf("failed to execute raft/trytoleader at host[%s], err: %s", req.Req.URL, err)
+	}
+	return nil
+}
---
+++ internal/xenon_request.go
@@ -0,0 +1,92 @@
+/*
+Copyright 2021 RadonDB.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package internal
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+
+	"github.com/radondb/radondb-mysql-kubernetes/utils"
+)
+
+// RequestConfig is the configuration required to get xenon http requests.
+type RequestConfig struct {
+	rootPassword string
+	host         string
+	xenonHttpUrl utils.XenonHttpUrl
+	data         interface{}
+}
+
+func NewRequestConfig(host, rootPasswd string, xenonHttpUrl utils.XenonHttpUrl, data interface{}) RequestConfig {
+	return RequestConfig{
+		rootPassword: rootPasswd,
+		host:         host,
+		xenonHttpUrl: xenonHttpUrl,
+		data:         data,
+	}
+}
+
+// NewXenonHttpRequest returns the http request of the corresponding type according to the incoming configuration.
+func NewXenonHttpRequest(cfg RequestConfig) (*Request, error) {
+	requestType, ok := utils.XenonHttpUrls[cfg.xenonHttpUrl]
+	if !ok {
+		return nil, fmt.Errorf("xenon http url[%s] does not exist or does not support", cfg.xenonHttpUrl)
+	}
+
+	switch requestType {
+	case http.MethodGet:
+		return newHttpGetRequest(cfg)
+	case http.MethodPost:
+		return newHttpPostRequest(cfg)
+	default:
+		return nil, fmt.Errorf("request type[%s] does not support", requestType)
+	}
+}
+
+func newHttpGetRequest(cfg RequestConfig) (*Request, error) {
+	req, err := http.NewRequest(http.MethodGet, newXenonRequestUrl(cfg.host, cfg.xenonHttpUrl), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	encoded := base64.StdEncoding.EncodeToString(append([]byte("root:"), cfg.rootPassword...))
+	req.Header.Set("Authorization", "Basic "+encoded)
+	return &Request{Req: req}, nil
+}
+
+func newHttpPostRequest(cfg RequestConfig) (*Request, error) {
+	reqBody := ""
+	if cfg.data != nil {
+		reqBody = cfg.data.(string)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, newXenonRequestUrl(cfg.host, cfg.xenonHttpUrl), bytes.NewBuffer([]byte(reqBody)))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	encoded := base64.StdEncoding.EncodeToString(append([]byte("root:"), cfg.rootPassword...))
+	req.Header.Set("Authorization", "Basic "+encoded)
+	return &Request{Req: req}, nil
+}
+
+func newXenonRequestUrl(host string, xenonHttpUrl utils.XenonHttpUrl) string {
+	return fmt.Sprintf("http://%s:%d%s", host, utils.XenonPeerPort, xenonHttpUrl)
+}
