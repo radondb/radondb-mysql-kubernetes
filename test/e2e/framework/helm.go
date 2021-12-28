1585d971fe8f0203ef7159844405eab8b2a71a28
runkecheng <1131648942@qq.com>
2021-12-28 16:51:22 +0800

    e2e: e2e framework. #344

---
+++ test/e2e/framework/helm.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2021 RadonDB.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package framework
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	. "github.com/onsi/gomega"
+)
+
+func HelmInstallChart(release, ns string) {
+	args := []string{
+		"install", release, "./" + TestContext.ChartPath,
+		"--namespace", ns,
+		"--values", TestContext.ChartValues, "--wait",
+		"--kube-context", TestContext.KubeContext,
+		"--set", fmt.Sprintf("manager.tag=%s", TestContext.OperatorImageTag),
+	}
+
+	cmd := exec.Command("helm", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	Expect(cmd.Run()).Should(Succeed())
+}
+
+func HelmPurgeRelease(release, ns string) {
+	args := []string{
+		"delete", release,
+		"--namespace", ns,
+		"--kube-context", TestContext.KubeContext,
+	}
+	cmd := exec.Command("helm", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	Expect(cmd.Run()).Should(Succeed())
+}
--- test/e2e/framework/test_context.go
+++ test/e2e/framework/test_context.go
@@ -18,82 +18,57 @@ package framework
 
 import (
 	"flag"
-	"fmt"
 	"os"
 
+	"github.com/onsi/ginkgo/config"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-const (
-	// TODO(gry): make sure the default port, do we need it?
-	defaultHost = "https://127.0.0.1:6443"
-
-	// DefaultNumNodes is the number of nodes. If not specified, then number of nodes is auto-detected
-	DefaultNumNodes = -1
-)
-
-// TestContextType contains test settings and global state. Due to
-// historic reasons, it is a mixture of items managed by the test
-// framework itself, cloud providers and individual tests.
-// The goal is to move anything not required by the framework
-// into the code which uses the settings.
-//
-// The recommendation for those settings is:
-// - They are stored in their own context structure or local
-//   variables.
-// - The standard `flag` package is used to register them.
-//   The flag name should follow the pattern <part1>.<part2>....<partn>
-//   where the prefix is unlikely to conflict with other tests or
-//   standard packages and each part is in lower camel case. For
-//   example, test/e2e/storage/csi/context.go could define
-//   storage.csi.numIterations.
-// - framework/config can be used to simplify the registration of
-//   multiple options with a single function call:
-//   var storageCSI {
-//       NumIterations `default:"1" usage:"number of iterations"`
-//   }
-//   _ config.AddOptions(&storageCSI, "storage.csi")
-// - The direct use Viper in tests is possible, but discouraged because
-//   it only works in test suites which use Viper (which is not
-//   required) and the supported options cannot be
-//   discovered by a test suite user.
-//
-// Test suite authors can use framework/viper to make all command line
-// parameters also configurable via a configuration file.
 type TestContextType struct {
+	KubeHost    string
 	KubeConfig  string
 	KubeContext string
-	Host        string
 
-	OutputDir string
+	ReportDir string
 
-	// If set to true test will dump data about the namespace in which test was running.
+	ChartPath   string
+	ChartValues string
+
+	OperatorImageTag string
+	SidecarImage     string
+
+	TimeoutSeconds    int
 	DumpLogsOnFailure bool
-	// Disables dumping cluster log from master and nodes after all tests.
-	DisableLogDump bool
-	TimeoutSeconds int
 }
 
-// TestContext should be used by all tests to access common context data.
 var TestContext TestContextType
 
-// RegisterCommonFlags registers flags common to all e2e test suites.
-// The flag set can be flag.CommandLine (if desired) or a custom
-// flag set that then gets passed to viperconfig.ViperizeFlags.
-//
-// The other Register*Flags methods below can be used to add more
-// test-specific flags. However, those settings then get added
-// regardless whether the test is actually in the test suite.
-//
-// For tests that have been converted to registering their
-// options themselves, copy flags from test/e2e/framework/config
-// as shown in HandleFlags.
-func RegisterCommonFlags(flags *flag.FlagSet) {
-	flags.StringVar(&TestContext.KubeConfig, clientcmd.RecommendedConfigPathFlag, os.Getenv(clientcmd.RecommendedConfigPathEnvVar), "Path to kubeconfig containing embedded authinfo.")
-	flags.StringVar(&TestContext.KubeContext, clientcmd.FlagContext, "", "kubeconfig context to use/override. If unset, will use value from 'current-context'")
-	flags.StringVar(&TestContext.Host, "host", "", fmt.Sprintf("The host, or apiserver, to connect to. Will default to %s if this argument and --kubeconfig are not set.", defaultHost))
-	flags.StringVar(&TestContext.OutputDir, "e2e-output-dir", "/tmp", "Output directory for interesting/useful test data, like performance data, benchmarks, and other metrics.")
-	flags.BoolVar(&TestContext.DumpLogsOnFailure, "dump-logs-on-failure", true, "If set to true test will dump data about the namespace in which test was running.")
-	flags.BoolVar(&TestContext.DisableLogDump, "disable-log-dump", false, "If set to true, logs from master and nodes won't be gathered after test run.")
-	flag.IntVar(&TestContext.TimeoutSeconds, "pod-wait-timeout", 100, "Timeout to wait for a pod to be ready.")
+// Register flags common to all e2e test suites.
+func RegisterCommonFlags() {
+	// Turn on verbose by default to get spec names
+	config.DefaultReporterConfig.Verbose = true
+
+	// Turn on EmitSpecProgress to get spec progress (especially on interrupt)
+	config.GinkgoConfig.EmitSpecProgress = true
+
+	// Randomize specs as well as suites
+	config.GinkgoConfig.RandomizeAllSpecs = true
+
+	flag.StringVar(&TestContext.KubeHost, "kubernetes-host", "", "The kubernetes host, or apiserver, to connect to")
+	flag.StringVar(&TestContext.KubeConfig, "kubernetes-config", os.Getenv(clientcmd.RecommendedConfigPathEnvVar), "Path to config containing embedded authinfo for kubernetes. Default value is from environment variable "+clientcmd.RecommendedConfigPathEnvVar)
+	flag.StringVar(&TestContext.KubeContext, "kubernetes-context", "", "config context to use for kuberentes. If unset, will use value from 'current-context'")
+
+	flag.StringVar(&TestContext.ReportDir, "report-dir", "", "Optional directory to store junit and pod logs output in. If not specified, no junit or logs files will be output")
+
+	flag.StringVar(&TestContext.ChartPath, "operator-chart-path", "../../charts/mysql-operator", "The chart name or path for mysql operator")
+	flag.StringVar(&TestContext.OperatorImageTag, "operator-image-tag", "latest", "Image tag for mysql operator.")
+	flag.StringVar(&TestContext.SidecarImage, "sidecar-image", "radondb/mysql-sidecar:latest", "Image path for mysql sidecar.")
+
+	flag.IntVar(&TestContext.TimeoutSeconds, "pod-wait-timeout", 1200, "Timeout to wait for a pod to be ready.")
+	flag.BoolVar(&TestContext.DumpLogsOnFailure, "dump-logs-on-failure", true, "Dump pods logs when a test fails.")
+}
+
+func RegisterParseFlags() {
+	RegisterCommonFlags()
+	flag.Parse()
 }
