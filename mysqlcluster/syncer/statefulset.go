c10dd5a065f66f4ccb0f21360cd84daa28dcb02d
runkecheng <1131648942@qq.com>
2022-06-01 18:14:24 +0800

    fix(cluster): update too many pods at the same time.

--- mysqlcluster/syncer/statefulset.go
+++ mysqlcluster/syncer/statefulset.go
@@ -36,6 +36,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -218,7 +219,7 @@ func (s *StatefulSetSyncer) doExpandPVCs(ctx context.Context) error {
 		if err := s.cli.Update(ctx, &item); err != nil {
 			return err
 		}
-		if err := retry(time.Second*2, time.Duration(waitLimit)*time.Second, func() (bool, error) {
+		if err := wait.PollImmediate(time.Second*2, time.Duration(waitLimit)*time.Second, func() (bool, error) {
 			// Check the pvc status.
 			var currentPVC corev1.PersistentVolumeClaim
 			if err2 := s.cli.Get(ctx, client.ObjectKeyFromObject(&item), &currentPVC); err2 != nil {
@@ -482,24 +483,31 @@ func (s *StatefulSetSyncer) applyNWait(ctx context.Context, pod *corev1.Pod) err
 		s.log.Info("pod is already updated", "pod name", pod.Name)
 	} else {
 		s.Status.State = apiv1alpha1.ClusterUpdateState
-		s.log.Info("updating pod", "pod", pod.Name)
-		if pod.DeletionTimestamp != nil {
-			s.log.Info("pod is being deleted", "pod", pod.Name)
-		} else {
-			// If healthy is always `yes`, retry() will exit in advance, which may
-			// cause excessive nodes are deleted at the same time, details: issue#310.
-			pod.ObjectMeta.Labels["healthy"] = "no"
-			if err := s.cli.Update(ctx, pod); err != nil {
-				return err
+		s.log.Info("updating pod", "pod", pod.Name, "key", s.Unwrap())
+		// Try to delete pod and wait for pod restart.
+		err := wait.PollImmediate(time.Second*5, time.Minute*5, func() (bool, error) {
+			if err := s.cli.Get(ctx, types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, pod); err != nil {
+				return false, nil
 			}
-			if err := s.cli.Delete(ctx, pod); err != nil {
-				return err
+			if pod.DeletionTimestamp != nil {
+				return false, nil
 			}
+			if pod.ObjectMeta.Labels["controller-revision-hash"] != s.sfs.Status.UpdateRevision {
+				if err := s.cli.Delete(ctx, pod); err != nil {
+					return false, err
+				}
+			} else {
+				return true, nil
+			}
+			return false, nil
+		})
+		if err != nil {
+			return err
 		}
 	}
 
 	// Wait the pod restart and healthy.
-	return retry(time.Second*10, time.Duration(waitLimit)*time.Second, func() (bool, error) {
+	return wait.PollImmediate(time.Second*10, time.Duration(waitLimit)*time.Second, func() (bool, error) {
 		err := s.cli.Get(ctx, types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, pod)
 		if err != nil && !k8serrors.IsNotFound(err) {
 			return false, err
@@ -532,39 +540,6 @@ func (s *StatefulSetSyncer) applyNWait(ctx context.Context, pod *corev1.Pod) err
 		}
 		return false, nil
 	})
-}
-
-// retry runs func "f" every "in" time until "limit" is reached.
-// it also doesn't have an extra tail wait after the limit is reached
-// and f func runs first time instantly
-func retry(in, limit time.Duration, f func() (bool, error)) error {
-	fdone, err := f()
-	if err != nil {
-		return err
-	}
-	if fdone {
-		return nil
-	}
-
-	done := time.NewTimer(limit)
-	defer done.Stop()
-	tk := time.NewTicker(in)
-	defer tk.Stop()
-
-	for {
-		select {
-		case <-done.C:
-			return fmt.Errorf("reach pod wait limit")
-		case <-tk.C:
-			fdone, err := f()
-			if err != nil {
-				return err
-			}
-			if fdone {
-				return nil
-			}
-		}
-	}
 }
 
 func basicEventReason(objKindName string, err error) string {
