52793b508c8ae42d28e0e10caf2fbe4cbd8a8594
runkecheng <1131648942@qq.com>
2022-08-19 16:09:19 +0800

    feat(e2e): add ci friendly cases

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2015 The Kubernetes Authors.
+Copyright 2022 RadonDB.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,29 +17,21 @@ limitations under the License.
 package e2e
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"path"
-	"time"
-
-	"strings"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	clientset "k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/radondb/radondb-mysql-kubernetes/test/e2e/framework"
-	"github.com/radondb/radondb-mysql-kubernetes/test/e2e/framework/ginkgowrapper"
 
 	// Test case source.
 	// Comment out the package that you don't want to run.
-	_ "github.com/radondb/radondb-mysql-kubernetes/test/e2e/cluster"
+	_ "github.com/radondb/radondb-mysql-kubernetes/test/e2e/install"
 	_ "github.com/radondb/radondb-mysql-kubernetes/test/e2e/simplecase"
 )
 
@@ -53,66 +45,16 @@ func TestE2E(t *testing.T) {
 }
 
 var _ = SynchronizedBeforeSuite(func() []byte {
-	kubeCfg, err := framework.LoadConfig()
-	Expect(err).To(Succeed())
-
-	c, err := client.New(kubeCfg, client.Options{})
-	if err != nil {
-		Fail(fmt.Sprintf("can't instantiate k8s client: %s", err))
-	}
-
-	By("Create Namespace")
-	operatorNsObj := &corev1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: framework.RadondbMysqlE2eNamespace,
-		},
-	}
-
-	if err := c.Create(context.TODO(), operatorNsObj); err != nil {
-		if !strings.Contains(err.Error(), "already exists") {
-			Fail(fmt.Sprintf("can't create mysql-operator namespace: %s", err))
-		}
-	}
-
 	if framework.TestContext.DumpLogs {
-		By("Create log dir")
+		By("create log dir")
 		os.MkdirAll(fmt.Sprintf("%s_%d", framework.TestContext.LogDirPrefix, GinkgoRandomSeed()), 0777)
 	}
-
-	By("Install operator")
-	framework.HelmInstallChart(framework.OperatorReleaseName, framework.RadondbMysqlE2eNamespace)
 	return nil
 }, func(_ []byte) {
 	framework.Logf("Running BeforeSuite actions on all node")
 })
 
-// Similar to SynchornizedBeforeSuite, we want to run some operations only once (such as collecting cluster logs).
-// Here, the order of functions is reversed; first, the function which runs everywhere,
-// and then the function that only runs on the first Ginkgo node.
-var _ = SynchronizedAfterSuite(func() {
-	// Run on all Ginkgo nodes.
-	framework.Logf("Running AfterSuite actions on all node")
-	framework.RunCleanupActions()
-
-	// Get the kubernetes client.
-	kubeCfg, err := framework.LoadConfig()
-	Expect(err).To(Succeed())
-
-	client, err := clientset.NewForConfig(kubeCfg)
-	Expect(err).NotTo(HaveOccurred())
-
-	By("Remove operator")
-	framework.HelmPurgeRelease(framework.OperatorReleaseName, framework.RadondbMysqlE2eNamespace)
-
-	By("Delete test namespace")
-	if err := framework.DeleteNS(client, framework.RadondbMysqlE2eNamespace, framework.DefaultNamespaceDeletionTimeout); err != nil {
-		framework.Failf(fmt.Sprintf("Can't delete namespace: %s", err))
-	}
-}, func() {
-	framework.Logf("Running AfterSuite actions on node 1")
-})
-
-var _ = ReportAfterSuite("Collect log", func(report Report) {
+var _ = ReportAfterSuite("collect log", func(report Report) {
 	if framework.TestContext.DumpLogs {
 		f, err := os.OpenFile(path.Join(fmt.Sprintf("%s_%d", framework.TestContext.LogDirPrefix, GinkgoRandomSeed()), "overview.txt"), os.O_RDWR|os.O_CREATE, 0644)
 		if err != nil {
@@ -137,8 +79,7 @@ var _ = ReportAfterSuite("Collect log", func(report Report) {
 // generated in this directory, and cluster logs will also be saved.
 // This function is called on each Ginkgo node in parallel mode.
 func RunE2ETests(t *testing.T) {
-	RegisterFailHandler(ginkgowrapper.Fail)
-
+	RegisterFailHandler(Fail)
 	// Fetch the current config.
 	suiteConfig, reporterConfig := GinkgoConfiguration()
 	// Whether printing FullTrace.
